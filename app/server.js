7b96137f3f27dfd7244b7f4637085bddbca23c0b
nasrullah878 <enasrullahkhan@gmail.com>
2026-04-06 22:37:25 +0500

    update

--- app/server.js
+++ app/server.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const client = require("prom-client");
 
 const app = express();
 const PORT = 4000;
 
+// collect default system metrics
+client.collectDefaultMetrics();
+
+// Home route
 app.get("/", (req, res) => {
-    res.send("HHello from DevOps CI/CD Pipeline 🚀");
+    res.send("Hello from DevOps CI/CD Pipeline 🚀");
 });
 
+// Health check route
 app.get("/health", (req, res) => {
     res.json({status:"OK"});
+});
+
+// Prometheus metrics endpoint
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", client.register.contentType);
+    res.end(await client.register.metrics());
 });
 
 app.listen(PORT, () => {
